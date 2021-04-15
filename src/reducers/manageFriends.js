8e855c8f662082cb0b89a031a02644f6da802995
chrislemus <icrislemus@gmail.com>
2021-04-14 21:16:06 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      const newFriendsList = [...state.friends, action.friend];
+      return { ...state, friends: newFriendsList };
+    case "REMOVE_FRIEND":
+      const filteredFriendsList = state.friends.filter(
+        (friend) => friend.id !== action.id
+      );
+      return { ...state, friends: filteredFriendsList };
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
-export function managePresents(state, action){
+export function managePresents(state = { numberOfPresents: 0 }, action) {
+  switch (action.type) {
+    case "INCREASE":
+      const newNum = state.numberOfPresents + 1;
+      return { numberOfPresents: newNum };
+    default:
+      return { numberOfPresents: 0 };
+  }
 }
